f119c760573a85f082f21682f3c911c03629b612
Michaelnim <64705346+Michaelnim@users.noreply.github.com>
2022-07-13 13:19:37 -0700

    Add files via upload

---
+++ part01-11_arithmetics/src/arithmetics.py
@@ -0,0 +1,12 @@
+# Write your solution here
+x = 27
+y = 15
+
+add = (x + y)
+sub = (x - y)
+multi = (x * y)
+divide = (x / y)
+print(f'{x} + {y} = {add}')
+print(f'{x} - {y} = {sub}')
+print(f'{x} * {y} = {multi}')
+print(f'{x} / {y} = {divide}')
