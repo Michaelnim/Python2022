0f65d06dddc3c934dd626f97f65322bcd6c3bd23
Michaelnim <64705346+Michaelnim@users.noreply.github.com>
2022-08-02 12:07:22 -0700

    Add files via upload

---
+++ part02-02_number_of_characters/src/number_of_characters.py
@@ -0,0 +1,10 @@
+# Write your solution here
+
+word = input("Please type in a word: ")
+
+if len(word) <= 1:
+    print("Thank you!")
+
+else:
+    print(f"There are {len(word)} letters in the word {word}")
+    print("Thank you!")
